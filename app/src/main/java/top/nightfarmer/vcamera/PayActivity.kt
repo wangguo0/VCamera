01890e9692e5f811aa51b9ef214c54322305e0bc
wangguo0 <122715114+wangguo0@users.noreply.github.com>
2026-07-13 10:04:47 +0800

    Update PayActivity.kt

--- app/src/main/java/top/nightfarmer/vcamera/PayActivity.kt
+++ app/src/main/java/top/nightfarmer/vcamera/PayActivity.kt
@@ -2,36 +2,39 @@ package top.nightfarmer.vcamera
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_pay.*
+import androidx.appcompat.app.AppCompatActivity
 import top.nightfarmer.vcamera.donate.AlipayDonate
-import android.content.Context.MODE_PRIVATE
-
 
 class PayActivity : AppCompatActivity() {
+
+    private lateinit var btAlipay: Button
+    private lateinit var btHidePay: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
-        //nightfarmer tsx09794xowzgzxocqfa3ec
-//        donateAlipay("tsx09794xowzgzxocqfa3ec");
+
+        // 初始化视图引用
+        btAlipay = findViewById(R.id.bt_alipay)
+        btHidePay = findViewById(R.id.bt_hide_pay)
 
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        title = "捐赠"
         supportActionBar?.title = "捐赠"
 
-        bt_alipay.setOnClickListener {
+        btAlipay.setOnClickListener {
             donateAlipay("tsx09794xowzgzxocqfa3ec")
         }
-        bt_hide_pay.setOnClickListener {
+
+        btHidePay.setOnClickListener {
             val sharedPreferences = getSharedPreferences("db", Context.MODE_PRIVATE)
-            val editor = sharedPreferences.edit()//获取编辑器
+            val editor = sharedPreferences.edit()
             editor.putBoolean("hideIcon", true)
-            editor.commit()//提交修改
-            Toast.makeText(this, "感谢使用", Toast.LENGTH_SHORT).show();
+            editor.commit()
+            Toast.makeText(this, "感谢使用", Toast.LENGTH_SHORT).show()
             finish()
         }
     }
@@ -43,9 +46,7 @@ class PayActivity : AppCompatActivity() {
 
     /**
      * 支付宝支付
-     * @param payCode 收款码后面的字符串；例如：收款二维码里面的字符串为 https://qr.alipay.com/tsx09794xowzgzxocqfa3ec ，则
-     * payCode = tsx09794xowzgzxocqfa3ec
-     * 注：不区分大小写
+     * @param payCode 收款码后面的字符串
      */
     private fun donateAlipay(payCode: String) {
         val hasInstalledAlipayClient = AlipayDonate.hasInstalledAlipayClient(this)
